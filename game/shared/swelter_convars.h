6bb874543766926b193c54a5a3b4635fc4278b4a
Eduard Rostovtsev <rostovtsev.eduard@gmail.com>
2023-05-18 23:41:28 +0300

    -swelter_convars.h - HEVcrab.

---
+++ game/shared/swelter_convars.h
@@ -0,0 +1,9 @@
+// ConVars introduced by SDE dev team.
+// HEVcrab to bring them all and in this header bind them ;)
+
+#include <convar.h>
+
+ConVar sde_weaponhint("sde_weaponhint", "0", FCVAR_ARCHIVE);
+ConVar sde_simple_alt_reload("sde_simple_alt_reload", "0", FCVAR_REPLICATED | FCVAR_ARCHIVE);
+ConVar sde_simple_rifle_bolt("sde_simple_rifle_bolt", "1", FCVAR_REPLICATED | FCVAR_ARCHIVE);
+ConVar sde_drop_mag("sde_drop_mag", "1", FCVAR_REPLICATED | FCVAR_ARCHIVE);
