733f9c624dd0e6923946fe61edbf95cabee5fdef
Vlad Melnichuk <vladislav.melnichuk@gmail.com>
2024-02-17 20:16:16 +0300

    Sawed-off shotgun both barrels fire now shoots twice as many pellets as single fire

--- game/server/hl2/weapon_sos.cpp
+++ game/server/hl2/weapon_sos.cpp
@@ -555,7 +555,7 @@ void CWeaponSOS::SecondaryAttack(void)
 	Vector vecAiming = pPlayer->GetAutoaimVector(AUTOAIM_SCALE_DEFAULT);
 
 	// Fire the bullets
-	pPlayer->FireBullets(12, vecSrc, vecAiming, GetBulletSpread(), MAX_TRACE_LENGTH, m_iPrimaryAmmoType, 0, -1, -1, 0, NULL, false, false);
+	pPlayer->FireBullets(sk_plr_num_shotgun_pellets.GetInt() * 2, vecSrc, vecAiming, GetBulletSpread(), MAX_TRACE_LENGTH, m_iPrimaryAmmoType, 0, -1, -1, 0, NULL, false, false);
 	pPlayer->ViewPunch(QAngle(random->RandomFloat(-8, -8), random->RandomFloat(-8, 8), 0));
 
 	pPlayer->SetMuzzleFlashTime(gpGlobals->curtime + 1.0);
